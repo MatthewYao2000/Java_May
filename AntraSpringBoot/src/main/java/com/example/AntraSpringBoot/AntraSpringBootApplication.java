e2d29c8c936bced3ce728d4535f20bd9bab92c57
pengfeiyao <matthew.yao.2000@gmail.com>
2023-05-22 14:42:48 -0500

    spring boot

---
+++ AntraSpringBoot/src/main/java/com/example/AntraSpringBoot/AntraSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.AntraSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntraSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntraSpringBootApplication.class, args);
+	}
+
+}
---
+++ AntraSpringBoot/src/test/java/com/example/AntraSpringBoot/AntraSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AntraSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntraSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
